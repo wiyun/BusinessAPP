6fbdb2c0a3250feb66b59eea55a0a567d297bf67
wiyun <1123871264@qq.com>
2017-05-12 14:47:20 +0800

    加入扫码

---
+++ app/src/main/java/com/administr/businessapp/ui/ScanQrActivity.java
@@ -0,0 +1,14 @@
+package com.administr.businessapp.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by a on 2017/5/12.
+ */
+public class ScanQrActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
