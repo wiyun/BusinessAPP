fb5e8528e60b5870a162e191d096e155faf0cf33
zhangxinhong <zhangxinhong@chinasofti.com>
2017-05-12 11:02:47 +0800

    提交代码

---
+++ app/src/main/java/com/administr/businessapp/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.administr.businessapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.administr.businessapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
